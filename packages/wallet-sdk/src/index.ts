286373f1ed3d79c8d23966609aacc1d54416500c
geovgy <gbh218@gmail.com>
2024-07-08 22:12:20 -0400

    Add connector to export

--- packages/wallet-sdk/src/index.ts
+++ packages/wallet-sdk/src/index.ts
@@ -2,4 +2,5 @@ import { AliasWalletProvider } from './provider';
 export default AliasWalletProvider;
 
 export { AliasWalletProvider } from './provider';
+export { connector } from './connector';
 export type { AppMetadata, Preference, ProviderInterface } from './core/provider/interface.ts';
